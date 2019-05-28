ab2c9163713c23736c65455d0ffcbcfde23c6894
Lisa Xu <50823583+ZeroxAlone@users.noreply.github.com>
2019-05-28 20:02:37 +0800

    Add files via upload

--- 190523.py
+++ 190523.py
@@ -1,3 +1,4 @@
+#week2 1 不对
 """
 Is your secret number 83?
 Enter 'h' to indicate the guess is too high.
@@ -40,7 +41,7 @@ print("Game over. Your secret number was:",c)
 """
 #question set
 #1
-
+'''
 b=eval(input("balance = "))
 a=eval(input("annualInterestRate = "))
 m=eval(input("monthlyPaymentRate = "))
@@ -58,9 +59,76 @@ for i in range(12):
 print("""Result Your Code Should Generate:
 -------------------
 Lowest Payment: """,z)
-
+'''
 #2不对 3不会
 
+#week 2 2
+#x**4
+'''
+def fourthPower(x):
+    return x**4
+x=eval(input(""))
+print(fourthPower(x))
+'''
+#odd
+'''
+def odd(x):
+    x:int
+    a=x%2
+    if a==1:
+        return True
+    if a==0:
+        return False
+x=eval(input(""))
+print(odd(x))
+'''
+#iterPower
+'''
+def iterPower(base,exp):
+    base:int or float
+    exp:int>=0
+    return base**exp
+base=eval(input(""))
+exp=int(input(""))
+print(iterPower(base,exp))
+'''
+#recurPower
+'''
+def recurPower(base, exp):
+    if exp==0:
+        return 1
+    return recurPower(base, exp-1)*base
+base=int(input(""))
+exp=int(input(""))
+print(recurPower(base,exp))
+'''
+#gcdIter
+'''
+a=int(input(""))
+b=int(input(""))
+c=min(a,b)
+l=[]
+def gcdIter(a,b):
+    for i in range(1,c+1):
+        if a%i==0 and b%i==0:
+            l.append(i)
+    return max(l)
+print(gcdIter(a,b))
+'''
+#gcdRecur 不对
+'''
+a=int(input(""))
+b=int(input(""))
+def gcdRecur(a,b):
+    d=max(a,b)
+    e=min(a,b)
+    c=d%e
+    if c==0:
+        return e
+    else:
+        gcdRecur(a,b)
+print(gcdRecur(a,b))
+'''
 
 
 
